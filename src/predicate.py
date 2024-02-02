9fcb7a0ba6bbabfcae9eef333808ce4233b13e28
k-onoue <k.onoue0724@gmail.com>
2024-02-02 15:39:09 +0900

    rewrite predicate for dual in heuristic approach

--- src/predicate.py
+++ src/predicate.py
@@ -1,4 +1,15 @@
 import numpy as np
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import auc
+import sklearn.metrics
+
+from sklearn.metrics import f1_score
+from scipy.optimize import minimize
+
+import optuna
+
+def pr_auc_score():
+    pass
 
 
 # from .setup_problem import Setup
@@ -16,7 +27,15 @@ class Setup_:
 
 
 class Predicate_dual:
-    def __init__(self, obj: Setup_, name: str, kernel_function: object = None) -> None:
+    def __init__(
+            self, 
+            obj: Setup_, 
+            name: str, 
+            kernel_function: object = None,
+            metrics: sklearn.metrics = None,
+            opt_iter_num: int = 100
+        ) -> None:
+    
         self.c1 = obj.c1
         self.c2 = obj.c2
 
@@ -46,15 +65,20 @@ class Predicate_dual:
         else:
             self.k = kernel_function
 
+        if metrics == None:
+            self.metrics = f1_score
+        else:
+            self.metrics = metrics
+
+        self.n_trials = opt_iter_num
+        
+
         ##########################################################
         ##########################################################
         ##########################################################
-        """ 
-        なぜか b += 0.5 するとうまく識別できている．（p の予測値に対して 0.5 が境界）
-        入力データで y in {-1, 1} であるからか
-        """
-        self.b = self._b() + 0.5
+
         # self.b = self._b()
+        self.b = self.optimize_b()
 
         self.w_linear_kernel = self._w_linear_kernel() 
 
@@ -64,33 +88,55 @@ class Predicate_dual:
     def linear_kernel(self, x1: np.ndarray, x2: np.ndarray) -> float:
         return np.dot(x1, x2)
     
+    def compute_kernel_matrix(self, X1, X2):
+        """
+        Compute the kernel matrix between two matrices.
+
+        Parameters:
+        - X1: First input matrix (n x m)
+        - X2: Second input matrix (n x m)
+        - kernel_function: Kernel function to use (default is dot product)
+
+        Returns:
+        - Kernel matrix (n x n)
+        """
+        kernel_function = self.k
+
+        K_matrix = kernel_function(X1, X2.T)
+        return K_matrix
+    
     def w_dot_phi(self, x_pred: np.ndarray) -> float:
-        k = self.k
-        value = 0
-
-        for l in range(self.len_l):
-            x = self.L[l, :-1]
-            y = self.L[l, -1]
-            lmbda = self.lambda_jl[l]
-            value += 2 * lmbda * y * k(x, x_pred)
-
-        for h in range(self.len_h):
-            for i in range(self.len_i):
-                lmbda = self.lambda_hi[h, i]
-                for u in range(self.len_u):
-                    x = self.U[u]
-                    M = self.M_j[h][i, u]
-                    value += - lmbda * M * k(x, x_pred)
+        values = np.zeros(len(x_pred))
         
-        for s in range(self.len_s):
-            x = self.S[s]
-            eta = self.eta_js[s]
-            eta_hat = self.eta_hat_js[s]
-            value += (eta - eta_hat) * k(x, x_pred)
+        x = self.L[:, :-1]
+        y = self.L[:, -1]
+        lmbda = self.lambda_jl
+        K = self.compute_kernel_matrix(x, x_pred)
 
-        return value
+        values += 2 * (y * lmbda).T @ K 
+
+        x = self.U
+        lmbda = self.lambda_hi.flatten().reshape(-1, 1)
+        M = np.vstack(self.M_j)
+        K = self.compute_kernel_matrix(x, x_pred)
+
+        values += (-1) * (lmbda * M @ K).sum(axis=0)
+
+        x = self.S
+        eta = self.eta_js
+        eta_hat = self.eta_hat_js
+        K = self.compute_kernel_matrix(x, x_pred)
+
+        values += (eta - eta_hat) @ K
+        
+        return values
+    
     
     def _w_linear_kernel(self) -> np.ndarray:
+        """
+        This should be rewritten using matrix calculations.
+        """
+        
         input_dim = len(self.L[0, :-1])
         w_linear_kernel = np.zeros(input_dim)
 
@@ -116,18 +162,57 @@ class Predicate_dual:
 
         return w_linear_kernel
     
-    def _b(self) -> float:
+    def _calculate_initial_b(self) -> float:
+        x = self.L[:, :-1]
+        y = self.L[:, -1]
 
-        value_tmp = 0
+        initial_b = np.mean(y - self.w_dot_phi(x))
 
-        for l in range(self.len_l):
-            x = self.L[l, :-1]
-            y = self.L[l, -1] 
-            value_tmp += y - self.w_dot_phi(x)
+        return initial_b
+    
+    def _calculate_score_at_b(
+            self, 
+            b: float,
+            X: np.ndarray,
+            y: np.ndarray
+        ) -> float:
 
-        value = value_tmp / self.len_l
+        y_pred = self.w_dot_phi(X)
 
-        return value
+        y_pred = np.array(y_pred) + b
+        y_pred_interpreted = np.where(y_pred >= 0.5, 1, -1)
+
+        score = self.metrics(y, y_pred_interpreted)
+
+        return score
+    
+    def optimize_b(self):
+        initial_b = self._calculate_initial_b()
+
+        X_train = self.L[:, :-1]
+        y_train = self.L[:, -1]
+
+        range_size = 5
+
+        min_bound = initial_b - 0.5 * range_size
+        max_bound = initial_b + 0.5 * range_size
+        
+        print()
+        print(f'min_bound: {min_bound}')
+        print(f'max_bound: {max_bound}')
+        print()
+
+        def objective(trial):
+            b = trial.suggest_float('b', min_bound, max_bound)
+            return self._calculate_score_at_b(b, X_train, y_train)  
+        
+        study = optuna.create_study(direction='maximize')
+        study.optimize(objective, n_trials=self.n_trials)
+        study.enqueue_trial(initial_b)
+
+        optimal_b = study.best_params['b']
+        return optimal_b
+
 
     def __call__(self, x_pred: np.ndarray) -> float:
         
